343a1cd3b87ec846a095c2bb1f6eae5ce22e9f3e
Reeves <reeves.abbas@gmail.com>
2022-09-08 15:25:05 -0400

    jest setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+    ],
   };
 };
