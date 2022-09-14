bbccb958ff998a942de162268037fd4c6309123f
Reeves <reeves.abbas@gmail.com>
2022-09-13 21:20:29 -0400

    react navigation screens setup

---
+++ src/screens/Onboarding/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+const AuthStack = createNativeStackNavigator();
+
+const OnboardingStack = () => {
+  return (
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthStack.Screen name={'SIGNIN'} component={SignIn} />
+      <AuthStack.Screen name={'SIGNUP'} component={SignUp} />
+    </AuthStack.Navigator>
+  )
+}
+
+export default OnboardingStack
---
+++ src/screens/index.tsx
@@ -0,0 +1,32 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from 'react'
+
+import { Home } from './Home';
+import OnboardingStack from './Onboarding';
+
+export type StackParams = {
+  HOME: undefined;
+  CREATE: undefined;
+  ENTRY: {title: string, body: string};
+  SIGNIN: undefined;
+  SIGNUP: undefined;
+}
+
+const Stack = createNativeStackNavigator<StackParams>();
+
+export const NavigatorStack = ({}) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+    return (
+      <NavigationContainer>        
+        { loggedIn ?
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name={'HOME'} component={Home} />
+          </Stack.Navigator>
+          :
+          <OnboardingStack /> 
+        }
+      </NavigationContainer>
+    );
+}
