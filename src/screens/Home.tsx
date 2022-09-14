e8b9ea8c97dabcfbc5db53c05c59bc79ff596220
Reeves <reeves.abbas@gmail.com>
2022-09-13 21:20:45 -0400

    home screen started

---
+++ src/screens/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+import Screen from '../components/Screen'
+
+interface HomeProps {
+
+}
+
+export const Home = ({}) => {
+
+    return (
+      <Screen style={{}}>
+        <Text style={{}}> Journal App in Progress </Text>
+      </Screen>
+    );
+}
