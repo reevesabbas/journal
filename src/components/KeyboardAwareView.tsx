f620a39157dbf1fad795b3b4bf619be621209669
Reeves <reeves.abbas@gmail.com>
2022-09-13 21:19:35 -0400

    keyboard aware view component

---
+++ src/components/KeyboardAwareView.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { KeyboardAvoidingView, Platform, ScrollView, ViewStyle } from 'react-native';
+import tw from '../../tailwind';
+
+
+interface KeyboardAwareViewProps {
+  style?: ViewStyle;
+  children: React.ReactNode;
+}
+
+const KeyboardAwareView: React.FC<KeyboardAwareViewProps> = ({ style, children, ...rest}) => {
+
+    return (
+      <KeyboardAvoidingView
+        style={tw`flex-1` && style}
+        {...rest}
+        behavior={Platform.select({ios: 'padding'})}
+      >
+        <ScrollView 
+          style={tw`flex-1`} 
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={tw`grow`}
+        >
+          {children}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
+}
+
+export default KeyboardAwareView
