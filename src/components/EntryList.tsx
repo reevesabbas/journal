d54a3c9367aa1f7733cb31518e9fa129935efd1b
Reeves <reeves.abbas@gmail.com>
2022-09-19 12:31:01 -0400

    started entry list

---
+++ src/components/EntryList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from 'react-native';
+
+interface EntryListProps {
+
+}
+
+export const EntryList: React.FC<EntryListProps> = ({}) => {
+  const entry = ({})
+  return (
+    <View>
+      
+    </View>
+  );
+}
