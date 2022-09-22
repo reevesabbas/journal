2e1e0cddf930cad14eb3afa44edc355137cf30cb
Reeves <reeves.abbas@gmail.com>
2022-09-22 15:54:40 -0400

    entry component

---
+++ src/components/Entry.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import tw from '../../tailwind';
+
+interface EntryProps {
+  title: string,
+  body: string,
+  date?: string,
+  onPress?: (param?: any) => void;
+}
+
+export const Entry: React.FC<EntryProps> = ({title, body, date, onPress}) => {
+  return (
+    <View style={tw`w-full mb-3`}>
+      <Text style={tw`h5 text-lavenderBlue text-right pb-0.5`}> 
+        {date} 
+      </Text>
+      <TouchableOpacity 
+        style={tw`bg-gray h-31 p-3 rounded-md overflow-hidden`}
+        onPress={onPress}
+      >
+        <View style={tw`flex-row justify-between h-8`}>
+          <Text style={tw`h4`}>
+            {title}
+          </Text>
+          <TouchableOpacity style={tw`p-2`}>
+            <Icon 
+              name='options'
+              size={17}
+              color={'#FFF'}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={tw`pl-2 w-5/6 h-20`}>
+          <Text style={tw`p2`}>
+            {body}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
