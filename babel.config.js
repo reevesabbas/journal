542ff9091e97b1cf4d9f224cb74b624867de2541
Reeves <reeves.abbas@gmail.com>
2022-09-28 17:22:25 -0400

    moved babel config back to own file, updated rn and installed other packages

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    "plugins": [
+      "babel-plugin-transform-typescript-metadata",
+      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+      ["@babel/plugin-proposal-class-properties", { "loose": true }],
+      "react-native-reanimated/plugin",
+    ],
+    presets: [
+      ['babel-preset-expo', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+    ],
+  };
+};
