ce2dd9a2661dfda8f2390fcc25db56abbb469dd6
Reeves <reeves.abbas@gmail.com>
2022-10-13 00:29:38 -0400

    styling changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,10 @@ module.exports = {
       screens: {},
       colors: {
         'night': '#12141C',
+        'dusk': '#171922',
         'gray': '#1B1E26',
         'lightGray': '#323643',
+        'purple': '#1A1E2C',
         'lavenderBlue': '#6067FF',
         'white': '#FFF',
         'red': '#d90000',
@@ -23,7 +25,7 @@ module.exports = {
   plugins: [
     plugin((({addUtilities}) => {
       addUtilities({
-        'content-container': `flex-1 mx-3`,
+        'content-container': `flex-1 px-3`,
         'h1': `tracking-0.1 font-medium text-4xl text-white`,
         'h2': `tracking-0.1 font-medium text-3xl text-white`,
         'h3': `tracking-0.1 font-medium text-2xl text-white`,
