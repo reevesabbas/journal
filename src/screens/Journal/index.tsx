8b9735e8783a57c824ef80bae6d50f644f04c365
Reeves <reeves.abbas@gmail.com>
2022-10-17 21:39:07 -0400

    moved file, moved some code

---
+++ src/screens/Journal/index.tsx
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useContext, useMemo, useState } from 'react'
+import { LogBox } from 'react-native';
+
+import { Home } from './Home';
+import { EntryView } from './EntryView';
+import { CreateEntry } from './CreateEntry';
+import { Entry, User } from '../../typeorm/entity';
+
+LogBox.ignoreLogs(['Non-serializable values were found in the navigation state'])
+
+export type StackParams = {
+  HOME: undefined;
+  CREATE: {entry?: Entry};
+  ENTRY: Entry;
+  ONBOARD: undefined;
+}
+
+const Stack = createNativeStackNavigator<StackParams>();
+export const NavigatorStack = ({}) => {
+    return (
+      <NavigationContainer>        
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name='HOME' component={Home} />
+          <Stack.Screen name='ENTRY' component={EntryView} />
+          <Stack.Screen name='CREATE' component={CreateEntry} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+}
