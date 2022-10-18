4e17e82a468cb692fda294440158b8fc86dc5782
Reeves <reeves.abbas@gmail.com>
2022-10-17 21:41:46 -0400

    ugly code with nonworking user context functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import { registerRootComponent } from "expo";
 import { View } from 'react-native';
 import { useDeviceContext } from "twrnc";
 
-import { NavigatorStack } from "./screens";
+import { NavigatorStack } from "./screens/Journal";
 import tw from "../tailwind";
+import { useState } from "react";
+import { UserContext } from "./UserContext";
+import { OnboardingScreens } from "./screens/Onboarding";
 
 function App() {
+  const [userId, setUserId] = useState('');
   useDeviceContext(tw)
+
   return (
     <View style={tw`flex-1`}>
-      <NavigatorStack />
+      <UserContext.Provider value={{userId, setUserId}}>
+        {
+          userId === '' 
+          ? 
+          <OnboardingScreens /> 
+          : 
+          <NavigatorStack /> 
+        }
+      </UserContext.Provider>
     </View>
   );
 }
