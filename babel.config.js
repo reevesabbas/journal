b2ccd1e6f10e65d36cebe30bde9991f1cd1e2532
Reeves <reeves.abbas@gmail.com>
2022-10-17 21:35:08 -0400

    added plugin to fix flatlist error

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     "plugins": [
+      "@babel/plugin-transform-flow-strip-types",
       "babel-plugin-transform-typescript-metadata",
       ["@babel/plugin-proposal-decorators", { "legacy": true }],
       ["@babel/plugin-proposal-class-properties", { "loose": true }],
