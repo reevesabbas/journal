816a1bb6ff89d1cecefd6c39b87bc73107d4b6f2
Reeves <reeves.abbas@gmail.com>
2022-10-18 18:59:35 -0400

    changed id to uuid

--- src/typeorm/entity/User.ts
+++ src/typeorm/entity/User.ts
@@ -3,8 +3,8 @@ import { Entry } from "./index";
 
 @Entity('user')
 export class User extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id?: number;
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
 
   @Column()
   username!: string;
